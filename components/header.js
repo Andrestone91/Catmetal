b0e1a215a610c744d7f2f4d14915cdeca1271d8f
Andres <andrestone9191@gmail.com>
2022-11-17 17:00:35 -0300

    tercer commit

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ function addHeader(ele) {
     headerEl.innerHTML = `
     <div class="contenedor">
     <div class="contenedor__img">
-    <img class="contenedor__img-logo" src="./assets/logo_catm.png">
+    <img class="contenedor__img-logo" src="./assets/Catmetal-logo.jpg">
     </div>
     <div></div>
     </div>
