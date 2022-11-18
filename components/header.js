10f445d3f05fc350ccc31b4b7af70a7a4ef45e2a
Andres <andrestone9191@gmail.com>
2022-11-18 16:55:04 -0300

    v0.4

--- components/header.js
+++ components/header.js
@@ -1,15 +1,47 @@
 function addHeader(ele) {
     const headerEl = document.createElement("div");
-    headerEl.innerHTML = `
-    <div class="contenedor">
-    <div class="contenedor__img">
-    <img class="contenedor__img-logo" src="./assets/Catmetal-logo-2.jpg">
-    </div>
-    <div>
-    <a href=".index.html"></a>asdasd</div>
+    headerEl.innerHTML = `<div class="header-contenedor">
+    <a href="./index.html">
+      <img src="./assets/Catmetal-logo-2.jpg" alt="" class="contenedor__img-logo " />
+    </a>
     <div></div>
     <div></div>
+    <a href="#nosotros" class="text-nav">Nosotros</a>
+    <a href="./servicios.html" class="text-nav">Servicios</a>
+    <a href="./contacto.html" class="text-nav">Contacto</a>
+    <button class="abre-ventana"></button>
+    <div class="ventana">
+      <div class="ventana__contenedor-boton">
+        <button class="ventana__cierra-ventana"></button>
+      </div>
+      <div class="ventana__contenido">
+        <a class="ventana__text" href="#nosotros">¿Quienes somos?</a>
+        <a class="ventana__text" href="#servicios">Servicios</a>
+        <a class="ventana__text" href="#contacto">Contacto</a>
+        <img class="ventana__cruz" src="./assets/cruz.svg" alt="" />
+      </div>
     </div>
-    `
-    ele.appendChild(headerEl)
+    <img class="burger" src="./assets/burger.png" alt="" />
+  </div>`;
+
+    ele.appendChild(headerEl);
+
+    const ventanaEl = document.querySelector(".ventana");
+    const cerrar = document.querySelector(".ventana__contenido").children
+
+    const botonAbreVentana = document.querySelector(".abre-ventana");
+
+    const botonCierraVentana = document.querySelector(".ventana__cierra-ventana");
+
+    botonAbreVentana.addEventListener("click", () => {
+        ventanaEl.style.display = "block";
+    });
+    botonCierraVentana.addEventListener("click", () => {
+        ventanaEl.style.display = "none";
+    });
+    for (const c of cerrar) {
+        c.addEventListener("click", () => {
+            ventanaEl.style.display = "none";
+        })
+    }
 }
