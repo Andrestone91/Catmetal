2d50d67bd94d90dca6ff447e7132babe3c04afca
Andres <andrestone9191@gmail.com>
2022-11-19 14:13:46 -0300

    v0.5

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,21 @@ function componentsReusables() {
     const footerEl = document.querySelector(".contenedor-footer")
     addFooter(footerEl)
 }
+function botonEvento() {
+    const botonVolver = document.querySelector(".btn__volver")
+    const botonEl = document.querySelector(".btn")
+    botonEl.addEventListener("click", () => {
+        botonVolver.style.display = "inline-block"
+    })
+    botonVolver.addEventListener("click", () => {
+        botonVolver.style.display = "none"
+    })
+
+}
 function main() {
     componentsReusables()
+    botonEvento()
     const bienvenidaEl = document.querySelector(".bienvenida")
     const bienvenidaCEl = document.querySelector(".bienvenida__contenedor")
-
 }
 main()
