227dc327503527a0fcb164ded833918bbfc0521f
Andres <andrestone9191@gmail.com>
2022-11-28 20:38:38 -0300

    v0.9.5

--- pages/index.js
+++ pages/index.js
@@ -20,5 +20,26 @@ function main() {
     botonEvento()
     const bienvenidaEl = document.querySelector(".bienvenida")
     const bienvenidaCEl = document.querySelector(".bienvenida__contenedor")
+
+    window.addEventListener("scroll", () => {
+        var widgetEl = document.getElementById("animacion__widget");
+        var posicionWidget = widgetEl.getBoundingClientRect().top;
+        console.log(posicionWidget);
+        var tamañoPantalla = window.innerHeight * 0.9;
+        console.log(tamañoPantalla);
+
+        if (posicionWidget < tamañoPantalla) {
+            widgetEl.style.animation = "mover 1s ease-out";
+        }
+
+        var headerEl = document.querySelector(".header-contenedor");
+        var icoEl = document.querySelector(".contacto__ico");
+        icoEl.classList.toggle("abajito", window.scrollY > 0);
+        var textNav = document.querySelector(".nav-custom").children;
+        headerEl.classList.toggle("abajo", window.scrollY > 0);
+        for (const t of textNav) {
+            t.classList.toggle("down", window.scrollY > 0);
+        }
+    });
 }
 main()
